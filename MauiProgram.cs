f650c725f3187a306baaf8a64e2afe7f67374a85
Mustafa Arikan <mustafaarikanmail.com>
2026-08-22 18:38:08 +0200

    LocalNotification Plugin für Benachrichtigungen und Autamtisierungen von diese hinzugefügt

--- MauiProgram.cs
+++ MauiProgram.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.Extensions.Logging;
+using Plugin.LocalNotification;
 
 namespace TodoApp
 {
@@ -9,6 +10,7 @@ namespace TodoApp
             var builder = MauiApp.CreateBuilder();
             builder
                 .UseMauiApp<App>()
+                .UseLocalNotification()
                 .ConfigureFonts(fonts =>
                 {
                     fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
