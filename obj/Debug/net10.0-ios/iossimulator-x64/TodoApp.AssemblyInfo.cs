f19925972d530ac3ffa44a523eeae39c01401dd1
Mustafa Arikan <mustafaarikanmail.com>
2026-08-26 13:10:55 +0200

    Blaue clickanzeiger in TaskDetail entfernt.

--- obj/Debug/net10.0-ios/iossimulator-x64/TodoApp.AssemblyInfo.cs
+++ obj/Debug/net10.0-ios/iossimulator-x64/TodoApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TodoApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+a8828d1db892aaab8112305dfc9f8af16afcdb6e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+3e80f252c5f98651003dda5b89ef95563ebdd98b")]
 [assembly: System.Reflection.AssemblyProductAttribute("TodoApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TodoApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
