c59b7555ec75f08a370c557bd4e35ba29f33c7e3
Mustafa Arikan <mustafaarikanmail.com>
2026-08-28 21:42:15 +0200

    App Namen und Beschreibungen für Android, Windows und iOS aktualisiert.

--- obj/Debug/net10.0-android/TodoApp.AssemblyInfo.cs
+++ obj/Debug/net10.0-android/TodoApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TodoApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+f9ff4db430e6fec6ae0c5011e5f77d22cf0c0a61")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+1a23a636f3a0c3b04a0cc14700c0c8a833c7d7b1")]
 [assembly: System.Reflection.AssemblyProductAttribute("TodoApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TodoApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
